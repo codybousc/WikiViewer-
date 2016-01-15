9cad3cadba95f17ac68b59afc9d47bdef3f5e18a
Cody Bouscaren <codybousc@gmail.com>
2016-01-15 17:11:45 -0500

    Successfully returns hardcoded jsonp query

--- controllers/ViewerController.js
+++ controllers/ViewerController.js
@@ -4,14 +4,15 @@ wikiViewer.controller('ViewerController', ['$scope','$http',
 function($scope, $http) {
   $scope.words = "aaaaa yeah "
 
-  $http({
-  method: 'GET',
-  url: 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content&format=json'
-}).then(function successCallback(response) {
-    $scope.allData = response.data;
-  }, function errorCallback(response) {
-    console.log(response);
-  });
+  var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content &format=json&callback=JSON_CALLBACK';
 
+  $http.jsonp(url).
+      success(function(data, status) {
+        $scope.data = data;
+        console.log( status, $scope.data)
+      }).
+      error(function(data, status, headers, config) {
+        console.log(status, data);
+      })
 
 }]);
