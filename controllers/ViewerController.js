5268aaa0bf92b01147f504cca727bc659aad5924
Cody Bouscaren <codybousc@gmail.com>
2016-01-16 13:50:50 -0500

    Returns list of items associated with search term

--- controllers/ViewerController.js
+++ controllers/ViewerController.js
@@ -2,14 +2,21 @@ wikiViewer = angular.module("wikiViewer", [])
 
 wikiViewer.controller('ViewerController', ['$scope','$http',
 function($scope, $http) {
-  $scope.words = "aaaaa yeah "
 
-  var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Main%20Page&prop=revisions&rvprop=content &format=json&callback=JSON_CALLBACK';
+  //Returning Some Type of Response
+  // var url = 'https://en.wikipedia.org/w/api.php?action=query&titles=Ford&prop=revisions&rvprop=content&rvlimit=max &format=json&callback=JSON_CALLBACK';
+  //Returns List of items associated with search term
+  
+  var url = 'https://en.wikipedia.org/w/api.php?action=opensearch&search=Ford&prop=revisions&rvprop=content&rvlimit=max &format=json&callback=JSON_CALLBACK';
 
   $http.jsonp(url).
       success(function(data, status) {
+        $scope.results = [];
         $scope.data = data;
-        console.log( status, $scope.data)
+        $scope.results = data;
+        console.log($scope.data)
+        console.log("results", $scope.results);
+
       }).
       error(function(data, status, headers, config) {
         console.log(status, data);
